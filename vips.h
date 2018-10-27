d5cdd35d7cc1b2a75a7a1e6436ac9a38f9be78b2
kishor <kishor@shopalyst.com>
2018-10-27 14:14:05 +0530

    Revert "vips 8.7 related fix."
    This reverts commit ae6900317822ec37d0f0e09dc7f5cacc7e7e14a3.

--- vips.h
+++ vips.h
@@ -177,9 +177,9 @@ vips_type_find_save_bridge(int t) {
 }
 
 int
-vips_rotate(VipsImage *in, VipsImage **out, double dangle) {
+vips_rotate(VipsImage *in, VipsImage **out, double angle) {
 	int rotate = VIPS_ANGLE_D0;
-	int ange = dangle;
+
 	angle %= 360;
 
 	if (angle == 45) {
